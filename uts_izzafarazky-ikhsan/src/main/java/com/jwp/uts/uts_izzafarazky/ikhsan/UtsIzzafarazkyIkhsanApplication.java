fea925e600a466380f2c850c75a218e62f4120f6
izzafarazky <ihazkylzy@gmail.com>
2025-11-05 16:04:19 +0700

    Add files via upload

---
+++ uts_izzafarazky-ikhsan/src/main/java/com/jwp/uts/uts_izzafarazky/ikhsan/UtsIzzafarazkyIkhsanApplication.java
@@ -0,0 +1,16 @@
+package com.jwp.uts.uts_izzafarazky.ikhsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsIzzafarazkyIkhsanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtsIzzafarazkyIkhsanApplication.class, args);
+        System.out.println("==========================================");
+        System.out.println(" UTS Java Web Programming - Soal C (Product)");
+        System.out.println(" Aplikasi berjalan di: http://localhost:8085/products");
+        System.out.println("==========================================");
+    }
+}
---
+++ uts_izzafarazky-ikhsan/src/test/java/com/jwp/uts/uts_izzafarazky/ikhsan/UtsIzzafarazkyIkhsanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwp.uts.uts_izzafarazky.ikhsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtsIzzafarazkyIkhsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
